52d02bdfe93429728388e00c79881cc3c4a21861
AlexeyEsipov <a_esipov@list.ru>
2022-07-01 14:59:07 +0300

    проект Кинотеатр - исправил ошибку в HallService

--- src/main/java/ru/job4j/cinema/service/HallService.java
+++ src/main/java/ru/job4j/cinema/service/HallService.java
@@ -13,15 +13,13 @@ public class HallService {
     private static final int ROW = 3;
     private static final int CELL = 3;
     private final TicketDBStore store;
-    private int[][][] hall;
 
     public HallService(TicketDBStore store) {
         this.store = store;
-        initHall();
     }
 
     public int[][][] getHall(int filmId) {
-        int[][][] result = hall;
+        int[][][] result = initHall();
         if (filmId != 0) {
             int row;
             int cell;
@@ -42,14 +40,15 @@ public class HallService {
        Третье размерение - массив из двух элементов, в котором
        элемент с индексом 0 - это номер ряда начиная с 1,
        элемент с индексом 1 - это номер места, начиная с 1   */
-    private void initHall() {
-        this.hall = new int[ROW][CELL][2];
+    private int[][][] initHall() {
+        int[][][] hall = new int[ROW][CELL][2];
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 hall[i][j][0] = i + 1;
                 hall[i][j][1] = j + 1;
             }
         }
+        return hall;
     }
 }
 
